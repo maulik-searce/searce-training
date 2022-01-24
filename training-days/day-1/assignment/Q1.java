dca8924c7fee1f6f63a8d3c32c62faff266a45fe
Maulik Pipaliya <maulik.pipaliya@searce.com>
2022-01-24 14:34:48 +0530

    Added Q1

---
+++ training-days/day-1/assignment/Q1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        reverseArray(arr);
+
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        sc.close();
+    }
+
+    public static void reverseArray(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+
+    }
+}
